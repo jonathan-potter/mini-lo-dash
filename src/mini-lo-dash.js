352bbd350e588bdd498c5820ded672a104ff145e
Jonathan Potter <jonathan.potter.business@gmail.com>
2014-11-17 16:53:17 -0800

    removes completed code so clone can be used for exercises

--- src/mini-lo-dash.js
+++ src/mini-lo-dash.js
@@ -1,83 +1,9 @@
 ;(function (root) {
-    var _;
+    var _ = root._ = (root._ || {});
 
-    _ = root._ = (root._ || {});
-
-    _.each = function (array, callback) {
-        var element, index;
-
-        for (index = 0; index < array.length; index++) {
-            element = array[index];
-
-            callback(element);
-        }
-    };
-
-    _.any = function (array, callback) {
-        var anyTruthy;
-
-        anyTruthy = false;
-        _.each(array, function (element) {
-            anyTruthy = anyTruthy || callback(element);
-        });
-
-        return !!anyTruthy;
-    };
-
-    _.map = function (array, callback) {
-        var index, mappedArray;
-
-        mappedArray = [];
-        for(index = 0; index < array.length; index++) {
-            element = array[index];
-
-            mappedArray.push(callback(element));
-        }
-
-        return mappedArray;
-    };
-
-    _.select = function (array, callback) {
-        var index, selectedElements;
-
-        selectedElements = [];
-        for (index = 0; index < array.length; index++) {
-            element = array[index];
-
-            if (callback(element)) {
-                selectedElements.push(element);
-            }
-        }
-
-        return selectedElements;
-    };
-
-    _.reject = function (array, callback) {
-        var index, selectedElements;
-
-        selectedElements = [];
-        for (index = 0; index < array.length; index++) {
-            element = array[index];
-
-            if (!callback(element)) {
-                selectedElements.push(element);
-            }
-        }
-
-        return selectedElements;
-    };
-
-    _.reduce = function (array, callback, initialValue) {
-        var accumulator, element, index;
-
-        accumulator = initialValue;
-        for (index = 0; index < array.length; index++) {
-            element = array[index];
-
-            accumulator = callback(accumulator, element);
-        }
-
-        return accumulator;
-    };
+    // start writing your code here
+    // _.each = function (array, callback) {
+    //     // implementation!
+    // };
 
 })(window);
